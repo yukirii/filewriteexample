7df47a92c693f334b643f1111f997901b31670c5
Yuki Kirii <1394098+yukirii@users.noreply.github.com>
2021-05-13 23:13:45 +0900

    Write file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
+const filename = 'data/file01.md';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile(filename, "utf-8", (err, data) => {
+    var content = "";
+
+    if (!err) {
+      content = data;
+    }
+
+    console.log(content);
+
+    res.render('index', { title: 'Express', content: content });
+  });
+});
+
+/* POST /write */
+router.post('/write', function(req, res, next) {
+  const content = req.body.content;
+  console.log(content);
+
+  fs.writeFile(filename, content, function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
